1b24c97162a168f0d8b41790e8aae2701d4926b5
16danjj <16danjj@gmail.com>
2025-03-12 11:34:51 +0100

    day8, part1

--- src/main.rs
+++ src/main.rs
@@ -8,121 +8,89 @@ use std::str::FromStr;
 use std::u64;
 
 struct Data {
-    equations : Vec<Equations>
-}
-
-#[derive(Debug)]
-struct Equations {
-    value : usize,
-    list : Vec<usize>
-}
-
-impl Equations {
-    fn solution(&self, part2: bool) -> Option<usize> {
-        let mut ops = vec!['+'; self.list.len() - 1];
-        loop {
-            let total = ops.iter().zip(&self.list[1..]).fold(self.list[0], |val, (op, num)| match op {
-                '+' => val + *num, 
-                '*' => val * *num,
-                '|' => {
-                    if *num < 10 {
-                        val * 10 + *num
-                    } else if *num < 100 {
-                        val * 100 + *num
-                    } else if *num < 1000 {
-                        val * 1000 + *num
-                    } else {
-                        panic!("Number greater than 1000")
-                    }
-                }
-                _ => {panic!("invalid op {op}")}
-
-            });
-
-            if total == self.value {
-                return Some(total)
-            }
-
-            let mut pos = ops.len() - 1;
-
-            if (part2) {
-                loop {
-                    if ops[pos] == '+' {
-                        ops[pos] = '*';
-                        break;
-                    } else if ops[pos] == '*' {
-                        ops[pos] = '|';
-                        break;
-                    }
-                    
-                    else if pos == 0 {
-                        return None;
-                    }
-    
-                    ops[pos] = '+';
-                    pos -= 1;
-                }
-            } else {
-                loop {
-                    if ops[pos] == '+' {
-                        ops[pos] = '*';
-                        break;
-                    }
-                    else if pos == 0 {
-                        return None;
-                    }
-    
-                    ops[pos] = '+';
-                    pos -= 1;
-                }
-            }
-        }
-
-    }
-}
-
-impl FromStr for Equations {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (left, right) = s.split_once(": ").unwrap();
-        let value = left.parse().unwrap();
-        let list = right.split(' ').map(|num| num.parse().unwrap()).collect();
-
-        Ok(Self { value, list })
-    }
+    grid : Vec<Vec<char>>, 
+    map : HashMap<char, Vec<(i64,i64)>>,
+    rows : usize, 
+    cols : usize
 }
 
 
 impl Data {
     fn new() -> Self {
-        Data { equations : Vec::new() }
+        Data { grid : Vec::new(), map : HashMap::new(), rows : 0, cols : 0}
     }
 
     fn parse(&mut self) {
-        let f = File::open("inputs\\input_day7.txt").unwrap();
+        let f = File::open("inputs\\input_day8.txt").unwrap();
         let reader = BufReader::new(&f);
 
-        self.equations = reader.lines().into_iter().map(|line| line.unwrap().parse().unwrap()).collect();
+        for line in reader.lines() {
+            self.grid.push(line.unwrap().chars().collect());
+        }
 
-        //println!("{:?}", self.equations);
+        self.rows = self.grid.len();
+        self.cols = self.grid[0].len();
+
+        for (row, line) in self.grid.iter().enumerate() {
+            for (col, ch) in line.iter().enumerate() {
+                if *ch != '.' {
+                    let entry = self.map.entry(*ch).or_default();
+                    entry.push((row as i64, col as i64));
+                }
+
+            }
+        }
     }
 
     fn part1(&mut self) -> usize {
         
-        //let mut total = 0;
-        //total
+        let mut total = HashSet::new();
 
-        self.equations.iter().filter_map(|x| x.solution(false)).sum::<usize>()
+        for list in self.map.values() {
+
+            for i in 0..list.len() - 1 {
+                for j in i+1..list.len() {
+                    let rise = list[j].0 - list[i].0;
+                    let run = list[j].1 - list[i].1;
+
+                    let p1 = (list[i].0 - rise, list[i].1 - run);
+                    let p2 = (list[j].0 + rise, list[j].1 + run);
+
+                    if self.in_bound(p1) {
+                        total.insert(p1);
+                    }
+
+                    if self.in_bound(p2) {
+                        total.insert(p2);
+                    }
+
+
+                }
+            }
+        }
+
+    
+        total.len()
+
+        
     }
 
     fn part2(&mut self) -> usize {
 
-        //let mut total = 0;
+        let mut total = 0;
 
-        //total
+        total
 
-        self.equations.iter().filter_map(|x|   x.solution(true)).sum::<usize>()  }
+    }
+
+    fn in_bound(&self, pos: (i64,i64)) -> bool {
+
+        if pos.0 >= 0 && pos.0 < self.rows as i64 && pos.1 >= 0 && pos.1 < self.cols as i64 {
+            return true
+        }
+
+        false
+    }
 
 }
 
