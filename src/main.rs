43e77bcc4eec036a2b0dc5706cfba56af73db025
16danjj <16danjj@gmail.com>
2025-05-09 12:09:59 +0200

    day18 - part1

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,13 @@ use std::collections::HashMap;
 use std::collections::HashSet;
 use std::str::FromStr;
 use std::u64;
+use pathfinding::prelude::astar;
 
 struct Data {
     list: Vec<(i64,i64)>
 }
 
-
+const DIRS: [(i64,i64);4] = [(0,1), (0,-1), (1,0), (-1,0)];
 
 impl Data {
     fn new() -> Self {
@@ -26,11 +27,21 @@ impl Data {
             self.list.push(line.unwrap().split_once(',').map(|(x,y)| (x.parse::<i64>().unwrap(), y.parse::<i64>().unwrap())).unwrap());
         }
 
-        //println!("{:?}", self.list);
+        //println!("{:?}", &self.list[0..20]);
  
     }
-    fn part1(&mut self) -> usize {
-        0
+    fn part1(&mut self) -> i64 {
+        let memory : HashSet<&(i64,i64)> = self.list.iter().take(1024).collect();
+        let Some((_path, cost)) = astar(&(0i64,0i64), |state| {
+            DIRS.iter().map(|dir| ((state.0 + dir.0, state.1 + dir.1), 1)).filter(|pos| !memory.contains(&pos.0))
+            .filter(|pos| {pos.0.0 >= 0 && pos.0.1 >= 0 && pos.0.0 <= 70 && pos.0.1 <= 70} ).collect::<Vec<_>>()
+        }, |state| (70 - state.0) + (70 - state.1), |state| *state == (70,70))
+        else {
+            panic!("No path found");
+        };
+        
+        cost
+
     }
 
     fn part2(&mut self) -> usize {
